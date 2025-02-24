643cb809df408b13ad8fe4be2e2e486a9b658ab5
Hiten1007 <hitengupta1005@gmail.com>
2025-02-24 11:49:22 +0530

    final commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv';
-import app from './app'; // Import the configured app
+import app from './app'; 
 import http from 'http';
 import { WebSocketServer } from 'ws';
 import { handleWebSocketConnection } from './websockets/websocket';
 import { Request } from 'express';
 
-// Load environment variables
 if (!process.env.RAILWAY_ENV) {
   dotenv.config();
 }
@@ -13,37 +12,28 @@ if (!process.env.RAILWAY_ENV) {
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-// Create WebSocket server
-export const wss = new WebSocketServer({ noServer: true }); // Use `noServer: true`
+export const wss = new WebSocketServer({ noServer: true }); 
 
-// Handle WebSocket connection
 wss.on('connection', (ws, req) => {
   console.log('New WebSocket connection');
   handleWebSocketConnection(ws, req as Request);
 });
 
-// Handle HTTP server upgrade event
 server.on('upgrade', (request, socket, head) => {
   const origin = request.headers.origin;
-  console.log('WebSocket upgrade request from origin:', origin);
 
-  // Allow connections only from the specified frontend origin
   if (origin === 'https://zap-me-frontend.vercel.app') {
     wss.handleUpgrade(request, socket, head, (ws) => {
       wss.emit('connection', ws, request);
     });
   } else {
-    console.log('WebSocket connection rejected: invalid origin', origin);
-    socket.destroy(); // Reject the connection
+    socket.destroy(); 
   }
 });
 
-// Start the server
 const startServer = async () => {
   try {
     server.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-      console.log(`WebSocket server running on wss://zapme-backend-production.up.railway.app`);
     });
   } catch (err) {
     console.error('Failed to start the server:', err);
